8d05761ee8d5d6f816e757946347ceff8a00c5ec
Maawan Ahmad <maawan18@gmail.com>
2022-01-17 23:58:16 -0800

    Added Deep and Shallow Copy

--- deepCopyShallowCopy.cpp
+++ deepCopyShallowCopy.cpp
@@ -6,6 +6,7 @@ class A{
     int val;
     char *st;
     A(int a , char ch[]){
+        cout << "Constructor Called " << endl;
         this->val = a;
         st = new char(100);
         strcpy(st , ch);
@@ -19,12 +20,21 @@ class A{
     void display(){
         cout << val << " " << st << endl;
     }
+    ~A(){
+        delete this->st;
+        cout << "Destructor Called and Memory Cleaned " << endl;
+     }
 };
 int main(){
     char ch[] = {'B','a','b','b','e','r','\0'};
+    A *dyanmic = new A(8899 , ch);
     A one(786 , ch);
     A second(one);
     second.st[0] = 'M';
+    dyanmic->display();
+    delete dyanmic;
+    // Asignmenet Operator
+    second = one;
     one.display();
     second.display();
     return 0;
