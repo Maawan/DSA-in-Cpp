8e93e4df360abf83a96d2ee3584f030fe66635eb
Maawan Ahmad <40340661+Maawan@users.noreply.github.com>
2023-03-09 16:02:44 +0530

    Updated ... Merge by Size method

--- Disjoint.java
+++ Disjoint.java
@@ -2,10 +2,13 @@ class Disjoint{
         int n;
         int[] parent;
         int[] rank;
+        int[] size;
         public Disjoint(int n){
             this.n = n;
             parent = new int[n];
             rank = new int[n];
+            size = new int[n];
+            Arrays.fill(size , 1);
             for(int i = 0 ; i < n ; i++){
                 parent[i] = i;
             }
@@ -28,7 +31,19 @@ class Disjoint{
                 rank[p1]++;
             } 
         }
+        private void mergeBySize(int node1 , int node2){
+            int p1 = find(node1);
+            int p2 = find(node2);
+            if(p1 == p2) return;
+            if(size[p1] > size[p2]){
+                parent[p2] = p1;
+                size[p2] += size[p1];
+            }else{
+                parent[p1] = p2;
+                size[p1] += size[p2];
+            }
+        }
         private boolean isConnected(int node1 , int node2){
             return find(node1) == find(node2);
         }
-}
+    }
