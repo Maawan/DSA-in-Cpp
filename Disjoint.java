cd78481129c0872121dec386c78007b9a3dd96f0
Maawan Ahmad <40340661+Maawan@users.noreply.github.com>
2023-03-10 00:21:03 +0530

    corrected Disjoint mergeBySize function !

--- Disjoint.java
+++ Disjoint.java
@@ -37,10 +37,10 @@ class Disjoint{
             if(p1 == p2) return;
             if(size[p1] > size[p2]){
                 parent[p2] = p1;
-                size[p2] += size[p1];
+                size[p1] += size[p2];
             }else{
                 parent[p1] = p2;
-                size[p1] += size[p2];
+                size[p2] += size[p1];
             }
         }
         private boolean isConnected(int node1 , int node2){
